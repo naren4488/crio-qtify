78ad6e1784c642a787edecc3842a923f7991755a
naren4488 <narendrakajla77@gmail.com>
2023-11-23 12:11:31 +0530

    margin added to logo

--- src/componenets/Logo/Logo.jsx
+++ src/componenets/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import LogoImage from './../../assets/logo.png'
+import style from "./Logo.module.css"
 
 export default function Logo() {
   return (
-    <div>
+    <div className={style.logo}>
         <img src={LogoImage} alt="logo" width={67}/>
     </div>
   )
